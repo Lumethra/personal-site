e5ceae8120a317158fa8b08d9298eb75534b90ba
Lumethra <maximilian.zhu@loewenrot.de>
2026-08-03 13:51:36 +0200

    implementing command handeling, weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee (do u notice the mental helth issues??)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,101 @@
 
 import { useEffect, useRef } from "react";
 import Image from "next/image";
+import commands from '@/assets/commands.json';
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const historyRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    inputRef.current?.focus();
+    function focus() {
+      inputRef.current?.focus();
+    }
+
+    focus();
+
+    window.addEventListener('keydown', focus);
+
+    return () => {
+      window.removeEventListener('keydown', focus);
+    };
   }, []);
 
   function handleBlur() {
     inputRef.current?.focus();
   };
 
+  function handleKeyDown(e: any) {
+    if (e.key === "Enter") {
+      const commandInput = inputRef.current;
+
+      const command = commandInput?.value.trim();
+      if (!command) return;
+
+      constructOutput(command);
+
+      if (commandInput) commandInput.value = "";
+    }
+  }
+
+  function constructOutput(command: any) {
+    const terminal = historyRef.current;
+    if (!terminal) return;
+
+    const findCommands = commands.find(function (item) {
+      return item.command.toLowerCase() === command.toLowerCase();
+    });
+
+    // if (findCommands == undefined) {
+    //   const commandLine = document.createElement('div');
+    //   commandLine.className = 'terminal-line';
+    //   commandLine.innerHTML = `<span class="prompt">❯</span> <span class="command error">${command}</span>`;
+    //   terminal.appendChild(commandLine);
+
+    //   const outputLine = document.createElement('div');
+    //   outputLine.className = 'terminal-line output';
+    //   outputLine.innerHTML = `<span class="output error">${command}: command not found...</span>`;
+    //   terminal.appendChild(outputLine);
+    // }
+
+    const ifError = findCommands ? "" : "error";
+    // const ifErrorOutptu = findCommands ? findCommands?.output : `${command}: command not found...`;
+    function getOutput() {
+      if (!findCommands) {
+        return `${command}: command not found...`;
+      }
+
+      if (findCommands?.isFunction) {
+        const outputFunction = new Function("commands", findCommands?.output);
+
+        return outputFunction(commands);
+      }
+
+      return findCommands?.output;
+    }
+
+    const commandLine = document.createElement('div');
+    commandLine.className = 'terminal-line';
+    commandLine.innerHTML = `<span class="prompt">❯</span> <span class="command ${ifError}">${command}</span>`;
+    terminal.appendChild(commandLine);
+
+    const outputLine = document.createElement('div');
+    outputLine.className = 'terminal-line output';
+    outputLine.innerHTML = `<span class="output ${ifError}">${getOutput()}</span>`;
+    terminal.appendChild(outputLine);
+
+    terminal.scrollTop = terminal.scrollHeight;
+  }
+
   return (
     <div>
-      <div>
-        hii
-      </div>
+      <div id="history" ref={historyRef}></div>
       <div id="input">
-        ╭─<span style={{ background: "#00ff00", color: "white" }}> ~ </span> <br />
+        ╭─<span style={{ background: "#00ff00", color: "blue" }}> ~ </span> <br />
         <div style={{ display: "inline-flex", alignItems: "center", width: "100%" }}>
           ╰─
           {/* we luve random ahh stack overflow posts */}
-          <input type="text" id="command-input" ref={inputRef} onBlur={handleBlur} />
+          <input type="text" id="command-input" ref={inputRef} onBlur={handleBlur} onKeyDown={handleKeyDown} />
         </div>
       </div>
     </div >
